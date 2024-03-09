98a7fdddb54a9364114a7da985598e39a058f31a
Ravneet Anand <anand.ravneet099@gmail.com>
2024-03-09 01:41:23 +0000

    Change to the directory where the code is located.

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -6,10 +6,8 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-
-# Create a directory for the Node.js application
-mkdir -p /home/ec2-user/passports-service
-cd /home/ec2-user/passports-service
+# Change to the directory where the code is located
+cd /var/www/html
 
 # Assuming Node.js and npm are already installed, install the npm packages
 npm install
