b6a5b3f93a92efa1364587f25dbd3ccdb6f64555
Ravneet Anand <anand.ravneet099@gmail.com>
2024-03-09 02:16:29 +0000

    Update pm2 path.

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -12,4 +12,4 @@ cd /var/www/html
 # Assuming Node.js and npm are already installed, install the npm packages
 npm install
 
-pm2 start npm --name "image scanner server" -- start
+~/.nvm/versions/node/v20.11.1/bin/pm2 start npm --name "image scanner server" -- start
