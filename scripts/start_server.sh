ca02b0bdbfb8e68bf7076bacf75187344dfea517
Ravneet Anand <anand.ravneet099@gmail.com>
2024-03-09 01:33:16 +0000

    Load nvm before install.

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+# Load NVM
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+
 # Create a directory for the Node.js application
 mkdir -p /home/ec2-user/imageServerApp
 cd /home/ec2-user/imageServerApp
